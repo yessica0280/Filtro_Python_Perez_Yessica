ab49186e592aa5bf58aaf06a69e73266d0660679
yessica0280 <yp80905@gmail.com>
2024-05-07 12:51:56 -0500

    filtro

---
+++ filtro.py
@@ -0,0 +1,87 @@
+import json 
+with open("movistar.json", "r")as openfile:
+    def abrirArchivo():
+        mijson=[]
+        json.load=json(openfile)
+        print(mijson)
+with open("movistar.json", "w") as outfile:
+    def guardarArchivo():
+        mijson=[]
+        json.dump=json(outfile)
+        print(mijson)
+
+def crearusuario(usuarios):
+    crearusuario.append=usuarios(usuario, nombre, apellido, direccion, numer_celular, plan)
+    Usuario: usuario
+    Nombre: nombre
+    Apellido: apellido
+    Direccion: direccion
+    NumeroCelular: numero_celular
+    Plan: plan
+
+
+def leerusuario(usuarios):
+    leerusuario(usuario, nombre, apelldio, direccion, numero_celular, plan)
+
+
+booleano = True
+print("----- MENU ADMINISTRADOR -----")
+print("1. crear usuarios")
+print("2. leer informacion del usuario")
+print("3. actualizar usuario")
+print("4. eliminar usuario")
+print("5. asignar categorias al usuario")
+print("6. salir")
+print("------------------------------")
+print("")
+usuarios=[]
+
+for i in usuarios:
+    abrirArchivo()
+    mijson=[]
+    if x == 1:
+        print("crear un usuario nuevo: ")
+        print("por favor puede ingresar el usuario del nuevo usuario: ")
+        print("por favor puede ingresar el nombre del nuevo usuario: ")
+        print("por favor puede ingresar el apellido del nuevo usuario: ")
+        print("por favor puede ingresar la direccion del nuevo usuario: ")
+        print("por favor puede ingresar el nuevo numero celular del usuario: ")
+        print("por favor puede ingresar el plan del nuevo usuario: ")
+        nuevo_usuario={
+             Usuario: usuario,
+             Nombre: nombre,
+             Apellido: apellido,
+             Direccion: direccion,
+             NumeroCelular: numero_celular,
+             Plan: plan,
+        }
+    elif x == 2:
+        print("leer la informacion del usuario: ")
+        Usuario: usuario
+        Nombre: nombre
+        Apellido: apellido
+        Direccion: Direccion
+        NumeroCelular: numero_celular
+        Plan: plan
+
+print("-----MENU GESTION DE SERVICIOS-----")
+print("1. internet")
+print("2. usuarios")
+print("3. nuevos clientes")
+print("4. salir")
+print("-----------------------------------")
+print("")
+x=1
+for i in ():
+    if x==1:
+        print("ver planes de internet")
+        plan=plan
+        precio=precio
+    elif x == 2:
+        print("ver la informacion de los clientes")
+        print("")
+
+    
+
+
+    
